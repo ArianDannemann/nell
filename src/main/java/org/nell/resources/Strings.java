cc4320ca2c20fa40547c55070ae4d8099e482160
Arian Dannemann <arian.dannemann@gmail.com>
2022-05-05 21:06:11 +0200

    what the f is this

--- src/main/java/org/nell/resources/Strings.java
+++
@@ -1,8 +0,0 @@
-package org.nell.resources;
-
-public class Strings
-{
-    public static String INVALID_SIGNAL_EXCEPTION = "invalid signal";
-    public static String INVALID_OUTPUT_EXCEPTION = "invalid output";
-    public static String NOT_SUPPORTED_EXCEPTION = "not supported";
-}
